002a6360da526109e38e8bd84fbcae77e73d5408
Андрей Макаров <makar3982@yandex.ru>
2026-01-08 14:46:55 +0300

    Add new file parts.py

--- game.py
+++ game.py
@@ -1,19 +1,5 @@
-class Board:
-    # Инициализировать игровое поле - список списков с пробелами.
-    # Пробелы - это пустые клетки.
-    def __init__(self):
-        self.board = [[' ' for _ in range(3)] for _ in range(3)]
-
-    # Метод, который обрабатывает ходы игроков.
-    def make_move(self, row, col, player):
-        self.board[row][col] = player
-
-    # Метод, который отрисовывает игровое поле.
-    def display(self):
-        for row in self.board:
-            print('|'.join(row))
-            print('-' * 5)
-
+# импорт класса игрового поля из файла parts.py
+from parts import Board
 
 # Создать игровое поле - объект класса Board.
 game = Board()
